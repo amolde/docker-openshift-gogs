79bdf9447ea4a2eb3ae0701f84e679c19b331303
Amol Deshmukh <a.deshmukh@northeastern.edu>
2020-05-14 07:34:11 -0400

    conclusion: ssh does not quite work quickly

--- create.sh
+++ create.sh
@@ -1,2 +1,9 @@
 oc apply -f openshift/gogs-persistent-template.yaml 
-oc -n gitt new-app gogs-persistent --param=GOGS_IMAGE=wkulhanek/gogs --param=GOGS_VERSION=11.86 --param=HOSTNAME=gogs-gitt.ep-test.northeastern.edu
+oc -n gitt new-app gogs-persistent \
+    --param=GOGS_IMAGE=wkulhanek/gogs \
+    --param=GOGS_VERSION=11.86 \
+    --param=HOSTNAME=gogs.ep-test.northeastern.edu \
+    --param=GOGS_VOLUME_CAPACITY=20Gi \
+    --param=DB_VOLUME_CAPACITY=10Gi \
+    --param=DATABASE_USER=gogs_db_user \
+    --param=SKIP_TLS_VERIFY=true \
